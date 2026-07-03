e5e81972aa09864c1d72223eb013400be088c53e
Gintaras <ssargass@gmail.com>
2026-07-03 20:33:35 +0100

    Fix robots sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://goblintechuk.uk/sitemap.xml",
+    sitemap: "https://www.goblintechuk.uk/sitemap.xml",
   };
 }
