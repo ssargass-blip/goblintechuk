48c46fde3e1945b2f1f3f56e71695b34696bed70
Gintaras <ssargass@gmail.com>
2026-07-04 01:13:08 +0100

    Add Ahrefs analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -78,6 +79,11 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <Script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="okxe69DlC4Ql8AxKUNrqkQ"
+          strategy="afterInteractive"
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
