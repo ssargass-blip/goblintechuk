913298485384e17052b15189a8c410e8f2680ad2
Gintaras <ssargass@gmail.com>
2026-07-13 11:46:19 +0100

    Render deal links in server HTML

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -1,43 +1,44 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const navLinkStyle = {
-  color: "#cfd6cb",
-  textDecoration: "none",
+  color: '#cfd6cb',
+  textDecoration: 'none',
 };
 
 export function SiteHeader() {
   return (
     <header
-      className="site-header"
+      className='site-header'
       style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "25px 40px",
-        borderBottom: "1px solid #2a2d35",
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '25px 40px',
+        borderBottom: '1px solid #2a2d35',
       }}
     >
       <Image
-        src="/goblin-icon.png"
-        alt="GoblinTechUK logo"
+        src='/goblin-icon.png'
+        alt='GoblinTechUK logo'
         width={256}
         height={171}
         style={{
-          height: "70px",
-          width: "auto",
+          height: '70px',
+          width: 'auto',
         }}
       />
 
-      <nav className="site-nav" style={{ display: "flex", gap: "25px" }}>
-        <a href="#home" style={navLinkStyle}>
+      <nav className='site-nav' style={{ display: 'flex', gap: '25px' }}>
+        <Link href='/' style={navLinkStyle}>
           Home
-        </a>
-        <a href="#deals" style={navLinkStyle}>
+        </Link>
+        <Link href='/#deals' style={navLinkStyle}>
           Deals
-        </a>
-        <a href="#about" style={navLinkStyle}>
+        </Link>
+        <Link href='/#about' style={navLinkStyle}>
           About
-        </a>
+        </Link>
       </nav>
     </header>
   );
